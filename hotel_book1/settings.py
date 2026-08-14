b756c6b0ba711ceb76fec965c179deaa3f9be5d6
Sachin Patil <sp3555733@gmail.com>
2026-08-14 22:50:49 +0530

    Configure Cloudinary media storage

--- hotel_book1/settings.py
+++ hotel_book1/settings.py
@@ -31,7 +31,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "cloudinary_storage",
     "cloudinary",
+
     "django.contrib.humanize",
 
     # Local apps
--- hotel_book1/urls.py
+++ hotel_book1/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
-from django.views.static import serve
 from django.views.generic import RedirectView
 
 urlpatterns = [
@@ -15,16 +12,8 @@ urlpatterns = [
     path("api/", include("api.urls")),
 ]
 
-# ✅ MAIN APP ROUTES (WITH LANGUAGE SUPPORT)
+# MAIN APP ROUTES
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('bookings.urls')),
 )
-
-# ✅ MEDIA FILES (DEV & PRODUCTION)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
